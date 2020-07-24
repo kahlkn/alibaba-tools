f734ea04afb5685f2a6d6873ba31ebbb864b8930
Kahle <kahlkn@gmail.com>
2020-07-24 15:00:30 +0800

    >> merge commit 2020.0724 version 1.0.0.20191020.beta

---
+++ src/main/java/alibaba/pay/AliPayProperties.java
@@ -0,0 +1,28 @@
+package alibaba.pay;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "alibaba.pay")
+public class AliPayProperties {
+    private Boolean enabled;
+    private List<AliPayConfig> configs;
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<AliPayConfig> getConfigs() {
+        return configs;
+    }
+
+    public void setConfigs(List<AliPayConfig> configs) {
+        this.configs = configs;
+    }
+
+}
